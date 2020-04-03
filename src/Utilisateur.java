3c1401baa04a3f2ae23a8d89640d5a8b72618500
Cheng Jun <d.cheng@live.fr>
2020-04-03 11:57:47 +0200

    Implémentation de la classe Utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -5,5 +5,40 @@
  *
  */
 public class Utilisateur {
+	
+	/* Propriétés */
+	private int id;
+	private String nom;
+	
+	
+	/* Constructeurs */
+	public Utilisateur () {
+		
+	}
+
+	
+	/* Getters et Setters */
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+	
 
 }//end class
